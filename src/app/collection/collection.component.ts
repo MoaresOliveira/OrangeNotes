357b30abbcf0c578a912b7759fb84efad3b036fa
MoaresOliveira <moaresoliveira@gmail.com>
2022-08-29 20:20:46 -0300

    Responsivity Collection view

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
+  boardActive: boolean = true;
+  class: string = 'collection board'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleList(){
+    if(this.boardActive){
+      this.boardActive = false;
+      this.class = 'collection list'
+    }else {
+      this.boardActive = true;
+      this.class = 'collection board'
+    }
+    console.log(this.class)
   }
 
 }
