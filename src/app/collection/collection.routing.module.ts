866e0d320aaa53718cf7712fc7f27a179620bdf9
MoaresOliveira <moaresoliveira@gmail.com>
2022-09-04 17:33:48 -0300

    Collection's routing module

--- src/app/collection/collection.routing.module.ts
+++ src/app/collection/collection.routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModalComponent } from '../shared/components/modal/modal.component';
 import { CollectionComponent } from './collection.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: CollectionComponent
+  },
+  {
+    path: ':id',
     component: CollectionComponent
   }
 ];
