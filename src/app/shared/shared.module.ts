34c3fbe4222ae8421d8b5dd39a363cfbc2da9477
MoaresOliveira <moaresoliveira@gmail.com>
2022-09-04 17:29:43 -0300

    Module of all shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { AddButtonComponent } from './components/add-button/add-button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { TextareaAutosizeDirective } from './directives/text-area-auto-resize.directive';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    AddButtonComponent
+    AddButtonComponent,
+    ModalComponent,
+    TextareaAutosizeDirective
   ],
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   exports: [
     HeaderComponent,
-    AddButtonComponent
+    AddButtonComponent,
+    ModalComponent,
+    TextareaAutosizeDirective
   ],
 })
 export class SharedModule {}
