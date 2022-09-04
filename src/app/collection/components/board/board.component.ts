54e5494490799c53f4c248c6a7259c08a20b91e4
MoaresOliveira <moaresoliveira@gmail.com>
2022-09-04 20:05:54 -0300

    Board component, non used code excluded

--- src/app/collection/components/board/board.component.ts
+++ src/app/collection/components/board/board.component.ts
@@ -1,23 +1,19 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { Collection, CollectionType } from '../../../interfaces/collection';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Collection } from '../../../interfaces/collection';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit, OnChanges {
+export class BoardComponent implements OnChanges {
 
   @Input() collection!: Collection;
   nothing: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("board init", this.collection)
     if (this.collection === undefined) {
       this.nothing = true;
     }else {
