60308d4862741d7f781d74d5043f235f5db1e8da
MoaresOliveira <moaresoliveira@gmail.com>
2022-09-04 17:03:10 -0300

    Integration with Collection Service, Listing all Collections retrieved from API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Collection } from '../interfaces/collection';
+import { CollectionService } from '../services/collection.service';
 
 @Component({
   selector: 'app-home',
@@ -8,82 +9,15 @@ import { Collection } from '../interfaces/collection';
 })
 export class HomeComponent implements OnInit {
 
-  collections: Collection[] = [
-    {
-      id: 1,
-      name: 'Back-end',
-      percent: 50
-    },
-    {
-      id: 2,
-      name: 'Front-end',
-      percent: 75
-    },
-    {
-      id: 3,
-      name: 'UX Design',
-      percent: 25
-    },
-    {
-      id: 4,
-      name: 'Data Science',
-      percent: 7
-    },
-    {
-      id: 5,
-      name: 'Database',
-      percent: 50
-    },
-    {
-      id: 6,
-      name: 'Cloud Computing',
-      percent: 75
-    },
-    {
-      id: 7,
-      name: 'DevOps',
-      percent: 25
-    },
-    {
-      id: 8,
-      name: 'Design Thinking',
-      percent: 7
-    },
-    {
-      id: 9,
-      name: 'Data Structure',
-      percent: 50
-    },
-    {
-      id: 10,
-      name: 'Agile',
-      percent: 75
-    },
-    {
-      id: 11,
-      name: 'Cyber Security',
-      percent: 25
-    },
-    {
-      id: 12,
-      name: 'Accessibility',
-      percent: 7
-    },
-    {
-      id: 13,
-      name: 'Mobile Development',
-      percent: 7
-    },
-    {
-      id: 14,
-      name: 'Blockchain',
-      percent: 7
-    }
-  ]
+  collections: Collection[] = []
 
-  constructor() { }
+  constructor(private collectionService: CollectionService) { }
 
   ngOnInit() {
+    this.collectionService.getCollections()
+      .subscribe(data => {
+        this.collections = data;
+      })
   }
 
 }
